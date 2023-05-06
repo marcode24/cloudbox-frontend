ec63426e94406cd1e26224559990e8c94922dee1
marco cruz <marco24cruz08@gmail.com>
2023-05-06 13:11:15 -0600

    :lipstick: add header auth

--- src/app/auth/auth.routing.ts
+++ src/app/auth/auth.routing.ts
@@ -1,18 +1,25 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { AuthComponent } from "./auth.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
- {
-  path: 'login',
-  component: LoginComponent
- },
- {
-  path: 'register',
-  component: RegisterComponent
- }
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+    ],
+  },
 ];
 
 @NgModule({
