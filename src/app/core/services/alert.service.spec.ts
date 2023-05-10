1cff4a5ea6456c4b49663641f0f2af30f7cb1fd1
marco cruz <marco24cruz08@gmail.com>
2023-05-10 12:59:59 -0600

    :white_check_mark: add test for alert service

--- src/app/core/services/alert.service.spec.ts
+++ src/app/core/services/alert.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
+import { IAlert } from '@interfaces/alert.interface';
+
 import { AlertService } from './alert.service';
 
 describe('AlertService', () => {
@@ -12,5 +14,16 @@ describe('AlertService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should emit an alert', () => {
+    const alert: IAlert = {
+      type: 'success',
+      message: 'Test alert',
+    };
+    service.alert.subscribe((data) => {
+      expect(data).toEqual(alert);
+    });
+    service.emitAlert(alert);
   });
 });
